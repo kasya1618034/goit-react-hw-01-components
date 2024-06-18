7b9735306790a3965d5cab03dca70c44a0765aa8
Katarzyna Filas <katarzyna.filas@gmail.com>
2024-06-18 21:43:47 +0200

    Add .json files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,33 @@
-import { Profile } from ".profile/Profile";
-import { Statistics } from ".statistics/Statistics";
-import { FriendList } from ".friend_list/FriendList";
-import { TransactionHistory } from ".transaction_history/TransactionHistory";
+import { Profile } from ".profile/Profile.jsx";
+import { Statistics } from ".statistics/Statistics.jsx";
+import { FriendList } from ".friend_list/FriendList.jsx";
+import { TransactionHistory } from ".transaction_history/TransactionHistory.jsx";
+
+import user from ".data/user.json";
+import data from ".data/data.json";
+import friends from ".data/friends.json";
+import transactions from ".data/transactions.json";
 
 export const App = () => {
   return (
     <div>
-      <Profile />
-      <Statistics />
-      <FriendList />
-      <TransactionHistory />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics
+        title="Upload stats"
+        stats={data}
+      />
+      <FriendList
+        friends={friends}
+      />
+      <TransactionHistory
+        items={transactions}
+      />
     </div>
   );
 };
