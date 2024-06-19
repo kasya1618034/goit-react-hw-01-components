b9a2c599f68b0f364b1f7fd8907d31eea05bfd0b
Katarzyna Filas <katarzyna.filas@gmail.com>
2024-06-19 17:44:18 +0200

    Add css modules files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,10 @@ import { Statistics } from './statistics/Statistics.jsx';
 import { FriendList } from './friend_list/FriendList.jsx';
 import { TransactionHistory } from './transaction_history/TransactionHistory.jsx';
 
-import user from '.data/user.json';
-import data from '.data/data.json';
-import friends from '.data/friends.json';
-import transactions from '.data/transactions.json';
+import user from './data/user.json';
+import data from './data/data.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 
 export const App = () => {
   return (
